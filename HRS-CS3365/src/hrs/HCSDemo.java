b0f1cbc699f440f0200ea37e9c21e82f6fb812cb
Lino Virgen <lino.virgen96@gmail.com>
2017-04-23 20:10:45 -0500

    Change file names and deleted old files
    The new java files have more standard names so we now what they are
    meant to do. Also I change table and column names since they old ones
    were merely tests and demos. Please check that those table names match
    yours.

--- HRS-CS3365/src/hrs/HCSDemo.java
+++ HRS-CS3365/src/hrs/HCSDemo.java
@@ -1,27 +1,23 @@
+package hcs;
 public class HCSDemo
 {
 	public static void main(String[] args)
 	{			
-		//ScheduleRecord test
-		ScheduleRecord scheduleRecord = new ScheduleRecord();
-		System.out.println(scheduleRecord.appointmentExists("Gelfond", "12/23/2017", "4:30"));
-		//scheduleRecord.createAppointment("Gelfond", "12/23/2017", "4:30","Juan");
-		//scheduleRecord.changeAppointment("Gelfond", "12/23/2017", "4:30","Juan","Watson","11/06/2017","2:30");
-		System.out.println(scheduleRecord.appointmentExists("Gelfond", "12/23/2017", "4:30"));
-		scheduleRecord.printAll();
+		//ScheduleHandler test
+		ScheduleHandler schedule = new ScheduleHandler();
+		System.out.println(schedule.appointmentExists("Gelfond", "12/23/2017", "4:30"));
+		//schedule.createAppointment("Gelfond", "12/23/2017", "4:30","Juan");
+		//schedule.changeAppointment("Gelfond", "12/23/2017", "4:30","Juan","Watson","11/06/2017","2:30");
+		System.out.println(schedule.appointmentExists("Gelfond", "12/23/2017", "4:30"));
+		schedule.printAll();
 		
-		//PatientAccount test
-		PatientAccount accounts = new PatientAccount();
+		//PatientAccountHandler test
+		PatientAccountHandler accounts = new PatientAccountHandler();
 		accounts.printAll();
 		System.out.println(accounts.accountExists(123456789));
 		//accounts.addAccount("Eva Williams", "151 University Amarillo, TX", "999-444-7777", "hereiseva@ttu.edu", 147258369);
 		accounts.setName("Pedro Infante", 123456789);
 		accounts.printAll();
-		accounts.treatmentRecord.printAll();
-		accounts.treatmentRecord.setWeight(70.0, 123456789, "04/16/2017");
-		accounts.treatmentRecord.printAll();
-		accounts.paymentRecord.printAll();
 		accounts.close();
 	}
-
 }
